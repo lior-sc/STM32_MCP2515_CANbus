2ec518d6c6ed0e4ae223d7fb90126cf180472ea5
lior Schwartz <liorsmail89@gmail.com>
2023-09-22 00:53:46 +0300

    change mcp_reset func

--- STM32_MCP2515_CANbus/MCP2515/Src/MCP2515.c
+++ STM32_MCP2515_CANbus/MCP2515/Src/MCP2515.c
@@ -22,8 +22,9 @@ HAL_StatusTypeDef MCP2515_reset()
 
 	HAL_GPIO_WritePin(MCP2515_CS_GPIO_Port, MCP2515_CS_Pin, GPIO_PIN_RESET);
 	ret = HAL_SPI_Transmit(&hspi1, (uint8_t *)MCP2515_CAN_RESET, 1, MCP2515_SPI_TIMEOUT);
-	HAL_Delay(100);
 	HAL_GPIO_WritePin(MCP2515_CS_GPIO_Port, MCP2515_CS_Pin, GPIO_PIN_SET);
+
+	HAL_Delay(100);
 
 	return ret;
 }
